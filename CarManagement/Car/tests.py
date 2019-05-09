f20ca29640e7c53081cbc5672022e1c86974bb64
Tanvir <streametanvir@gmail.com>
2019-05-08 23:42:58 -0400

    test2

--- CarManagement/Car/tests.py
+++ CarManagement/Car/tests.py
@@ -16,16 +16,41 @@ class ShowRoomOwnerTestCase(TestCase):
         new_client = APIClient()
         res = new_client.get('/showroomowner/', kwargs={'pk': 5}, format="json")
         self.assertEqual(res.status_code, status.HTTP_404_NOT_FOUND)
+
     def test_get_all_showroomowner(self):
         new_client = APIClient()
         res = new_client.get('/showroomowner/', format="json")
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+
     def test_get_all_showroom(self):
         new_client = APIClient()
         res = new_client.get('/showroom/', format="json")
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+
     def test_get_all_car(self):
         new_client = APIClient()
         res = new_client.get('/allcardetail/', format="json")
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_get_all_carassigntoshowroom(self):
+        new_client = APIClient()
+        res = new_client.get('/carassigntoshowroom/', format="json")
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_get_all_showroomownerassigntoshowroom(self):
+        new_client = APIClient()
+        res = new_client.get('/showroomownerassigntoshowroom/', format="json")
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_get_all_showroomownerassigntoshowroom(self):
+        new_client = APIClient()
+        res = new_client.get('/showroomownerassigntoshowroom/', format="json")
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_get_single_getcarbyshowroominfo(self):
+        new_client = APIClient()
+        res = new_client.get('/getcarbyshowroom/', kwargs={'showroom': 1}, format="json")
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+   
    
