1d7a2cb455564ff6add575365c5254355bea25f6
Tanvir Islam Streame <streametanvir@gmail.com>
2019-06-08 21:15:02 +0600

    removing python env

--- CarManagement/envname/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/media/streame/VIDEO/Ubuntu/Untitled Folder/Untitled Folder/DjangoReactCarManagement/CarManagement/envname/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
