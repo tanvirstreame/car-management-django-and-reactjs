a06b5187f15f11866c6d6237586319bdf90e1c3e
tanvirstreame <engr.tanvir.islam.1@gmail.com>
2021-07-16 14:44:14 +0600

    [FIX] Card size

--- carmanagement_client/src/Components/getAllCar.js
+++ carmanagement_client/src/Components/getAllCar.js
@@ -28,43 +28,53 @@ class CarGetAll extends Component {
       <Dashboard
         title="Car List"
       >
-        <div className='container card  mb-5'>
+        <div className='container mb-5'>
           <div className="row">
             {this.state.car.length > 0 ?
-            this.state.car && this.state.car.map((rowData, i) => (
-              <Link key={rowData.id} className="linkelement" to={`/getcarinfo/${rowData.id}`}>
-                <div className="card">
-                  <div className="card-body">
-                    <h4 className="text-center">{rowData.name}</h4>
-                    <div className="carousel-inner">
-                      {rowData.image_feild.map((subRow, k) =>
-                        (k === 0) ?
-                          <div key={subRow.image} className="carousel-item active">
-                            <img src={subRow.image} className="" alt="..." />
-                          </div> :
-                          <div key={subRow.image} className="carousel-item">
-                            <img src={subRow.image} className="" alt="..." />
-                          </div>
-                      )}
-                    </div>
-                    <label><b>Id:</b></label>
-                    <span>{rowData.id}</span>
-                    <br />
-                    <label><b>Manufacture:</b></label>
-                    <span>{rowData.manufacture}</span>
-                    <br />
-                    <label><b>Mileage:</b></label>
-                    <span>{rowData.mileage}</span>
-                    <br />
-                    <label><b>Year:</b></label>
-                    <span>{rowData.year}</span>
-                    <br />
-                    <label><b>Status:</b></label>
-                    <i className="fas fa-building"></i><span>{rowData.status === 0 ? "Sold" : "In Stock"}</span>
+              this.state.car && this.state.car.map((rowData, i) => (
+                <div className="col-md-4 mb-3">
+                  <div className="card">
+                    <Link key={rowData.id} className="linkelement" to={`/getcarinfo/${rowData.id}`}>
+
+                      <div className="card-body">
+                        <h4 className="text-center">{rowData.name}</h4>
+                        <div className="carousel-inner">
+                          {rowData.image_feild.map((subRow, k) =>
+                            (k === 0) ?
+                              <div key={subRow.image} className="carousel-item active">
+                                <img src={subRow.image} className="" alt="..." />
+                              </div> :
+                              <div key={subRow.image} className="carousel-item">
+                                <img src={subRow.image} className="" alt="..." />
+                              </div>
+                          )}
+                        </div>
+                        <div className="mt-3">
+                          <label><b>Id:</b></label>
+                          <span>{rowData.id}</span>
+                        </div>
+                        <div>
+                          <label><b>Manufacture:</b></label>
+                          <span>{rowData.manufacture}</span>
+                        </div>
+                        <div>
+                          <label><b>Mileage:</b></label>
+                          <span>{rowData.mileage}</span>
+                        </div>
+                        <div>
+                          <label><b>Year:</b></label>
+                          <span>{rowData.year}</span>
+                        </div>
+                        <div>
+                          <label><b>Status:</b></label>
+                          <i className="fas fa-building"></i><span>{rowData.status === 0 ? "Sold" : "In Stock"}</span>
+                        </div>
+                      </div>
+
+                    </Link>
                   </div>
                 </div>
-              </Link>
-            )) : <div className="text-center w-100 mt-3"><p className="pb-3">No Data</p></div>}
+              )) : <div className="text-center w-100 mt-3"><p className="pb-3">No Data</p></div>}
           </div>
         </div>
       </Dashboard>
